489b71305998c45c6d1a915158a9af3c88a1dfa5
MoniIgnacio <moniignacio02@gmail.com>
2022-10-21 17:55:58 +0400

    add profile session

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,41 +1,93 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const User = require("../models/User.model.js");
+const bcrypt = require("bcryptjs");
 
-module.exports = router;
 
 // GET /auth/signup - render to user signup
 router.get("/signup", (req, res, next) => {
-    res.render("auth/signup.hbs")
-})
+  res.render("auth/signup.hbs");
+});
 
-router.post('/signup', (req, res, next) => {
-    const { username, surname, email, password } = req.body;
+// POST /auth/signup - render to user signup/create user
+router.post("/signup", async (req, res, next) => {
+  const { name, surname, email, phone, password, passwordConfirmation } =
+    req.body;
+  // 1. Guard clause.
+  if (
+    name === "" ||
+    surname === "" ||
+    email === "" ||
+    password === "" ||
+    passwordConfirmation === ""
+  ) {
+    res.render("auth/signup.hbs", {
+      errorMessage: "Introducir caracteres",
+    });
+    return;
+  }
+  // 2. Password validation.
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+  if (passwordRegex.test(password) === false) {
+    res.render("auth/signup.hbs", {
+      errorMessage: "Introducir caracteres validos",
+    });
+    return;
+  }
+  // 2.1 Password confirmation.
+  if (passwordConfirmation !== password) {
+    res.render("auth/signup.hbs", {
+      errorMessage: "the password confirmation must be same than password",
+    });
+    return;
+  }
+  // 2.2 User confirmation.
+  try {
+    const foundUser = await User.findOne({ name: name });
+    if (foundUser !== null) {
+      res.render("auth/signup.hbs", {
+        errorMessage: "El usuario ingresado ya existe con su nombre",
+      });
+      return;
+    }
+    // 3. Generate salt & hash password.
+    const salt = await bcrypt.genSalt(12);
+    const hashPassword = await bcrypt.hash(password, salt);
+    // 4. Create User.
+    let newUser = {
+      name: name,
+      surname: surname,
+      email: email,
+      phone: phone,
+      password: hashPassword,
+    };
 
-    if ( !username === '' || surname){}
-
-
-})
+    await User.create(newUser);
+    res.redirect("/auth/login");
+  } catch (error) {
+    next(error);
+  }
+});
 
 // GET /auth/login - render to user login
 router.get("/login", (req, res, next) => {
-    res.render("auth/login.hbs")
-})
+  res.render("auth/login.hbs");
+});
 
 // POST /auth/login - render to user login
-router.post("/auth/login", async (req, res, next) => {
-   const { email, password } = req.body
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
 
-    //1. Backend validations
-    //1.1 Fields completed
-    if ( !email || !password ) {
-        res.render("auth/login.hbs", {
-          errorMessage: "usuario ya creado con ese nombre",
-        });
-        return;
-      }
+  //1. Backend validations
+  //1.1 Fields completed
+  if (!email || !password) {
+    res.render("auth/login.hbs", {
+      errorMessage: "usuario ya creado con ese nombre",
+    });
+    return;
+  }
 
-      //1.2 Account exist
+  //1.2 Account exist
   try {
     const foundUser = await User.findOne({ email: email });
     if (foundUser === null) {
@@ -72,3 +124,7 @@ router.get("/logout", (req, res, next) => {
     res.redirect("/");
   });
 });
+
+
+module.exports = router;
+
