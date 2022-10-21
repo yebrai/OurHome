b3b36590e4fc7ade8dd44b4376b7fccc4d9a734c
yebrai <ivansevv@gmail.com>
2022-10-21 11:27:28 +0200

    added home/signup views and get

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,3 +3,8 @@ const router = express.Router();
 
 
 module.exports = router;
+
+// GET /auth/signup - render to user signup
+router.get("/signup", (req, res, next) => {
+    res.render("auth/signup.hbs")
+})
