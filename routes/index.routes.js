d920d025abf1d5a1d6eb70b1ae803f8e93650d7d
MoniIgnacio <moniignacio02@gmail.com>
2022-10-21 12:32:17 +0400

    adding  some views .hbs

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,4 +6,10 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+const authRoutes = require ('./auth.routes.js');
+router.use('/auth', authRoutes);
+
+const profileRoutes = require ('./profile.routes.js');
+router.use('/profile', profileRoutes);
+
 module.exports = router;
