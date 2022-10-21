e2eae64555d778ed2af82d161c64c8501165c35c
MoniIgnacio <moniignacio02@gmail.com>
2022-10-21 14:25:00 +0400

    create .post -/signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,18 @@
 const express = require ('express');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
 
 module.exports = router;
 
 // GET /auth/signup - render to user signup
 router.get("/signup", (req, res, next) => {
     res.render("auth/signup.hbs")
+})
+
+router.post('/signup', (req, res, next) => {
+    const { username, surname, email, password } = req.body;
+
+    
 })
 
 // GET /auth/login - render to user login
