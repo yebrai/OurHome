c775fc814264a4c42ac1e83891ace9abac6c189f
MoniIgnacio <moniignacio02@gmail.com>
2022-10-22 16:57:46 +0400

    fixed form select option link V. 1.0

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -11,11 +11,7 @@ router.use('/auth', authRoutes);
 const profileRoutes = require ('./profile.routes.js');
 router.use('/profile', profileRoutes);
 
-const authProfessionalRoutes = require ('./auth.professional.routes.js');
-router.use('/auth-professional', authProfessionalRoutes);
-
-const ProfessionalRoutes = require ('./professional.routes.js');
-router.use('/professional', ProfessionalRoutes);
-
+const professionalRoutes = require ('./professional.routes.js');
+router.use('/professional', professionalRoutes);
 
 module.exports = router;
--- routes/professional.routes.js
+++ routes/professional.routes.js
@@ -3,7 +3,7 @@ const { isLoggedIn, isAdmin, isProfessional } = require('../middlewares/auth');
 
 
 // GET /profile/list - render to user signup
-router.get("/list", isLoggedIn, (req, res, next) => {
+router.get("/list", isProfessional, (req, res, next) => {
     res.render("professional/list.hbs");
   });
 
