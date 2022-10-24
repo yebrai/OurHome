021c59dace84f10b0cebe1b21426fb6b685fc64c
MoniIgnacio <moniignacio02@gmail.com>
2022-10-24 21:43:15 +0400

    add owner information in house-create.hbs

--- routes/property.routes.js
+++ routes/property.routes.js
@@ -2,17 +2,29 @@ const router = require("express").Router();
 const { isLoggedIn, isAdmin, isProfessional } = require("../middlewares/auth");
 const User = require("../models/User.model");
 const Property = require("../models/Property.model");
-const uploader = require ('../middlewares/cloudinary.js');
-const cloudinary = require("../middlewares/cloudinary.js")
-
+const cloudinary = require("../middlewares/cloudinary.js");
 
 // GET /property/create - render to user create
-router.get("/create", isLoggedIn, (req, res, next) => {
-    res.render("property/house-create.hbs");
-  });
+router.get("/create", isLoggedIn, async (req, res, next) => {
+  try {
+    // antes de renderizar, voy a buscar todos los autores de la BD
+    const userList = await User.find();
+    console.log(userList);
+    res.render("property/house-create.hbs"),
+      {
+        userList,
+      };
+  } catch (error) {
+    next(error);
+  }
+});
 
 // POST /property/create - render to property create
-router.post("/create", isLoggedIn, cloudinary.single("property-img"), async (req, res, next) => {
+router.post(
+  "/create",
+  isLoggedIn,
+  cloudinary.single("property-img"),
+  async (req, res, next) => {
     const {
       name,
       location,
@@ -31,7 +43,7 @@ router.post("/create", isLoggedIn, cloudinary.single("property-img"), async (req
       });
       return;
     }
-  
+
     try {
       let newProperty = {
         name,
@@ -45,62 +57,64 @@ router.post("/create", isLoggedIn, cloudinary.single("property-img"), async (req
         price,
         professional,
       };
-  
+
       await Property.create(newProperty);
       res.redirect("/property/list");
     } catch (error) {
       next(error);
     }
-  });
+  }
+);
 
 // GET /property/list - render to user signup
 router.get("/list", isLoggedIn, async (req, res, next) => {
-    try {
-      let listProperties = await Property.find();
+  try {
+    let listProperties = await Property.find();
     //   console.log(listProperties);
-      res.render("property/list.hbs", {
-        listProperties,
-      });
-    } catch (error) {
-      next(error);
-    }
-  });
+    res.render("property/list.hbs", {
+      listProperties,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // GET '/property/details/:id' - render property-details
-router.get('/details/:propertyId', isLoggedIn, (req,res,next) => {
-    const {propertyId} = req.params
-    
-    Property.findById(propertyId)
+router.get("/details/:propertyId", isLoggedIn, (req, res, next) => {
+  const { propertyId } = req.params;
+
+  Property.findById(propertyId)
     .then((details) => {
       console.log(details);
       // console.log(details);
-      res.render('property/details.hbs',{
-        details
-      })
+      res.render("property/details.hbs", {
+        details,
+      });
     })
     .catch((err) => {
-      next(err)
-    })
-})
+      next(err);
+    });
+});
 
 // GET '/property/edit/:propertyId'
-router.get('/edit/:propertyId', isLoggedIn, (req,res,next) => {
-  let {propertyId} = req.params
+router.get("/edit/:propertyId", isLoggedIn, (req, res, next) => {
+  let { propertyId } = req.params;
 
   Property.findById(propertyId)
-  .then((details) => {
-    res.render('property/edit-property.hbs', {
-      details})
-  })
-  .catch((err) => {
-    next(err)
-  })
-
-})
+    .populate("owner")
+    .then((details) => {
+      res.render("property/edit-property.hbs", {
+        details,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 // POST '/property/edit/:propertyId'
-router.post('/edit/:propertyId', isLoggedIn, (req,res,next) => {
-  let {propertyId} = req.params
+router.post("/edit/:propertyId", isLoggedIn, (req, res, next) => {
+  let { propertyId } = req.params;
   const {
     name,
     location,
@@ -129,34 +143,28 @@ router.post('/edit/:propertyId', isLoggedIn, (req,res,next) => {
 
   console.log(req.body);
   Property.findByIdAndUpdate(propertyId, propertyUpdate)
-  .then(() => {
-    res.redirect(`/property/details/${propertyId}`)
-  })
-  .catch((err) => {
-    next(err)
-  })
-})
+    .then(() => {
+      res.redirect(`/property/details/${propertyId}`);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 // POST '/property/delete/:propertyId'
-router.post('/delete/:propertyId', isLoggedIn, (req,res,next) => {
-
+router.post("/delete/:propertyId", isLoggedIn, (req, res, next) => {
   Property.findByIdAndDelete(req.params.propertyId)
-  .then(() => {
-    res.redirect('/property/list')
-  })
-  .catch((err) => {
-    next(err)
-  })
-})
-
+    .then(() => {
+      res.redirect("/property/list");
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 // router.post('/:propertyId/favourite', isLoggedIn, (req,res,next) => {
 //   let {propertyId} = req.params
 
-
 // })
-
-
-
 
 module.exports = router;
