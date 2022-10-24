b8b26f24070a6947fb81cb158b8d86ec746d8f9e
yebrai <ivansevv@gmail.com>
2022-10-24 16:21:24 +0200

    adding cloudinary

--- routes/professional.routes.js
+++ routes/professional.routes.js
@@ -3,6 +3,19 @@ const { isLoggedIn, isAdmin, isProfessional } = require('../middlewares/auth');
 const Professional = require("../models/Professional.model.js");
 const Property = require("../models/Property.model");
 
+// GET /professional/list - render to user signup
+router.get("/list", isProfessional, async (req, res, next) => {
+  try {
+    let listProperties = await Property.find();
+  //   console.log(listProperties);
+    res.render("professional/list.hbs", {
+      listProperties,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 // GET /professional/profile
 router.get('/profile', isProfessional,(req,res,next) => {
   Professional.findById(req.session.professionalOnline._id)
@@ -17,18 +30,6 @@ router.get('/profile', isProfessional,(req,res,next) => {
 })
 
 
-// GET /professional/list - render to user signup
-router.get("/list", isProfessional, async (req, res, next) => {
-  try {
-    let listProperties = await Property.find();
-  //   console.log(listProperties);
-    res.render("professional/list.hbs", {
-      listProperties,
-    });
-  } catch (error) {
-    next(error);
-  }
-});
 
 
 
